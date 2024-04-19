83f1a10f9c5a54026740cc341acbb8108029b3d3
Jayda-Emily Pizzuco <2281161@edu.vaniercollege.qc.ca>
2024-04-19 14:59:32 -0400

    finalized database

--- movietheatre.sql
+++ movietheatre.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 19, 2024 at 05:41 PM
+-- Generation Time: Apr 19, 2024 at 08:58 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -26,24 +26,14 @@ USE `movietheatre`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Table structure for table `genre`
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE `admin` (
-  `admin_id` int(11) NOT NULL DEFAULT 1,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password_hash` varchar(60) NOT NULL
+DROP TABLE IF EXISTS `genre`;
+CREATE TABLE `genre` (
+  `genre_id` int(11) NOT NULL,
+  `genre_title` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`admin_id`, `username`, `email`, `password_hash`) VALUES
-(1, 'Admin', 'admin@email.com', '$2y$10$MsCHBzNjxAYz//fk8NwwgejQDX8VtWEfN2LNm4HjHaCExH4Eac7bi'),
-(1, 'Admin2', 'admin2@email.com', '$2y$10$HHBbEy.LoSMGzR4pirV6au1k/ZR.t7qTLaneOmvTYGbaflkCu536e');
 
 -- --------------------------------------------------------
 
@@ -55,7 +45,11 @@ DROP TABLE IF EXISTS `movie`;
 CREATE TABLE `movie` (
   `movie_id` int(11) NOT NULL,
   `title` varchar(50) NOT NULL,
-  `description` varchar(200) NOT NULL,
+  `image` varchar(200) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `length` int(11) NOT NULL,
+  `director` varchar(50) NOT NULL,
+  `trailer` varchar(500) NOT NULL,
   `release_date` date NOT NULL,
   `ticket_revenue` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -63,14 +57,99 @@ CREATE TABLE `movie` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `profile`
+-- Table structure for table `movie_genre`
 --
 
-DROP TABLE IF EXISTS `profile`;
-CREATE TABLE `profile` (
-  `profile_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `movie_genre`;
+CREATE TABLE `movie_genre` (
+  `movie_id` int(11) NOT NULL,
+  `genre_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie_schedule`
+--
+
+DROP TABLE IF EXISTS `movie_schedule`;
+CREATE TABLE `movie_schedule` (
+  `schedule_id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL,
+  `day` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL
+  `order_date` date NOT NULL,
+  `total_price` double NOT NULL,
+  `number_tickets` int(11) NOT NULL,
+  `order_status` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE `review` (
+  `review_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL,
+  `stars` int(11) NOT NULL,
+  `review_text` varchar(500) NOT NULL,
+  `review_date` datetime NOT NULL,
+  `approved` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seat`
+--
+
+DROP TABLE IF EXISTS `seat`;
+CREATE TABLE `seat` (
+  `seat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+CREATE TABLE `ticket` (
+  `ticket_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL,
+  `seat_id` int(11) NOT NULL,
+  `movie_day` date NOT NULL,
+  `movie_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `times`
+--
+
+DROP TABLE IF EXISTS `times`;
+CREATE TABLE `times` (
+  `time_id` int(11) NOT NULL,
+  `schedule_id` int(11) NOT NULL,
+  `time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -82,14 +161,21 @@ CREATE TABLE `profile` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password_hash` varchar(60) NOT NULL
+  `password_hash` varchar(60) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`genre_id`);
 
 --
 -- Indexes for table `movie`
@@ -98,11 +184,57 @@ ALTER TABLE `movie`
   ADD PRIMARY KEY (`movie_id`);
 
 --
--- Indexes for table `profile`
+-- Indexes for table `movie_genre`
 --
-ALTER TABLE `profile`
-  ADD PRIMARY KEY (`profile_id`),
+ALTER TABLE `movie_genre`
+  ADD KEY `movie_id` (`movie_id`),
+  ADD KEY `genre_id` (`genre_id`);
+
+--
+-- Indexes for table `movie_schedule`
+--
+ALTER TABLE `movie_schedule`
+  ADD PRIMARY KEY (`schedule_id`),
+  ADD KEY `movie_id` (`movie_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
   ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`review_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `movie_id` (`movie_id`);
+
+--
+-- Indexes for table `seat`
+--
+ALTER TABLE `seat`
+  ADD PRIMARY KEY (`seat_id`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`ticket_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `movie_id` (`movie_id`),
+  ADD KEY `seat_id` (`seat_id`),
+  ADD KEY `movie_day` (`movie_day`),
+  ADD KEY `movie_time` (`movie_time`);
+
+--
+-- Indexes for table `times`
+--
+ALTER TABLE `times`
+  ADD PRIMARY KEY (`time_id`),
+  ADD KEY `schedule_id` (`schedule_id`);
 
 --
 -- Indexes for table `user`
@@ -115,22 +247,102 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `movie`
 --
 ALTER TABLE `movie`
   MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `profile`
+-- AUTO_INCREMENT for table `movie_schedule`
 --
-ALTER TABLE `profile`
-  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `movie_schedule`
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `seat`
+--
+ALTER TABLE `seat`
+  MODIFY `seat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `times`
+--
+ALTER TABLE `times`
+  MODIFY `time_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `movie_genre`
+--
+ALTER TABLE `movie_genre`
+  ADD CONSTRAINT `movie_genre_ibfk_1` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`genre_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `movie_genre_ibfk_2` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `movie_schedule`
+--
+ALTER TABLE `movie_schedule`
+  ADD CONSTRAINT `movie_schedule_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ticket_ibfk_3` FOREIGN KEY (`seat_id`) REFERENCES `seat` (`seat_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `times`
+--
+ALTER TABLE `times`
+  ADD CONSTRAINT `times_ibfk_1` FOREIGN KEY (`schedule_id`) REFERENCES `movie_schedule` (`schedule_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

cb6d14210700ea1eb701d49f6a6475ea1710187f
Jayda Pizzuco <jaydapizzuco@gmail.com>
2024-04-21 15:48:55 -0400

    fixed error with AdminLogin filter, normal users are now directed to their profiles

--- app/filters/AdminLogin.php
+++ app/filters/AdminLogin.php
@@ -8,13 +8,19 @@ class AdminLogin implements \app\core\AccessFilter{
 		if(isset($_SESSION['user_id'])){
 			$user = new \app\models\User();
 			$status = $user->isAdmin($_SESSION['user_id']);
-			if($status == 1)
-			return true;	
+			if($status == 1){
+				return true;
+			}
+			else{
+			 header('location:/User/profile');
+			return false;		
 		}
+	}
 		else{
 			header('location:/User/login');
 			return false;
 		}
+			
 }
 
 }

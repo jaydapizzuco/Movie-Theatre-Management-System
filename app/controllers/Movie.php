c81e8ca89cecaa5592505d36072592120ddc1e14
yumvinegar <horse.shoe@outlook.com>
2024-04-20 22:55:22 -0400

    added functions to display all movies & individual movies in Movie controller

--- app/controllers/Movie.php
+++ app/controllers/Movie.php
@@ -6,20 +6,26 @@ class Movie extends \app\core\Controller{
 
     public function index(){
         $allMovies = new \app\models\Movie();
-        $allMovies = $allMovies->
+        $allMovies = $allMovies->getAll();
+        $this->view('Movie/index', $allMovies);
+    }
+
+    public function individual(){
+        $movie = new \app\models\Movie();
+        $movie = $movie->getByID($_GET['movie_id']);
+        $_SESSION['movie_id'] = $movie->movie_id;
+        $this->view('Movie/individual', $movie);
+    }
+
+    
+    public function create(){
+        
     }
 
     public function update(){
 
 
     }
-
-
-    public function createMovie(){
-
-
-    }
-
 
     public function delete(){
 

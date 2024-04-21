2a7bb9e4db7d158281a498f6dad69e23f53673ba
yumvinegar <horse.shoe@outlook.com>
2024-04-20 22:09:21 -0400

    added update function in Movie model

--- app/models/Movie.php
+++ app/models/Movie.php
@@ -30,7 +30,17 @@ class Movie extends \app\core\Model{
 	}
 
 	public function update(){
-
+		$SQL = 'UPDATE movie SET title=:title,image=:image,description=:description,length=:length,director=:description,trailer=:trailer,release_date:release_date WHERE movie_id=:movie_id';
+		$STMT = self::$_conn->prepare($SQL);
+		$STMT->execute(
+			['title'=>$this->title,
+			'image'=>$this->image,
+			'description'=>$this->description,
+			'length'=>$this->length,
+			'director'=>$this->director,
+			'trailer'=>$this->trailer,
+			'release_date'=>$this->release_date]
+		);
 	}
 
 	public function delete(){

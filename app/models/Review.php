cf058b820e3a8d6ba02c86f1128c03856b25ba51
yumvinegar <horse.shoe@outlook.com>
2024-04-20 22:40:42 -0400

    added insert method to Review model

--- app/models/Review.php
+++ app/models/Review.php
@@ -1,22 +1,53 @@
 <?php 
 namespace app\models;
 
-class Review {
+use PDO;
 
-  
+class Review extends \app\core\Model{
 
+	public $review_id;
+	public $user_id;
+	public $movie_id;
+	public $stars;
+	public $review_text;
+	public $review_date;
+	public $approved;
+
+	public function insert(){
+		$SQL = 'INSERT INTO review(user_id,movie_id,stars,review_text) VALUE (:user_id,:movie_id,:stars,:review_text';
+		$STMT = self::$_conn->prepare($SQL);
+		$STMT->execute(
+			['user_id'=>$this->user_id,
+			'movie_id'=>$this->movie_id,
+			'stars'=>$this->stars,
+			'review_text'=>$this->review_text]
+		);
+	}
+
+	public function update(){
+		
+	}
+
+	public function delete(){
+		
+	}
+
+	public function approve(){
+		
+	}
 
 	public function submitForApproval(){
 		
 	}
- 
-	public function create(){
+
+    public function getByUser($user_id){
 		
 	}
 
-    public function getByTitle(){
+	public function getByMovie($movie_id){
 		
 	}
+
     public function getAll(){
 		
 	}

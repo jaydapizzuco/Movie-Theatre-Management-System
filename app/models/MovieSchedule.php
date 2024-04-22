ba662aef67fa429b4934ea3d70ba68815c0ea9a4
Jayda Pizzuco <jaydapizzuco@gmail.com>
2024-04-22 18:19:06 -0400

    added insert function to MovieSchedule model

--- app/models/MovieSchedule.php
+++ app/models/MovieSchedule.php
@@ -4,8 +4,17 @@ namespace app\models;
 use PDO;
 
 class MovieSchedule extends \app\core\Model{
+	$schedule_id;
+	$movie_id;
+	$day;
 
-
+	public function insert(){
+		$SQL = 'INSERT INTO movie_schedule(movie_id,day) VALUES (:movie_id,day)';
+		$STMT = self::$_conn->prepare($SQL);
+		$data = ['movie_id'=>$this->movie_id,
+			'day'=>$this->day];
+		$STMT->execute($data);
+	}
 
 	public function addAdmin(){
 		

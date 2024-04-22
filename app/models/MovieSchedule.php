cffbaf36053c2db45d5d6b8b309a4a0224149dc1
Jayda Pizzuco <jaydapizzuco@gmail.com>
2024-04-22 18:21:22 -0400

    added delete function to MovieSchedule model

--- app/models/MovieSchedule.php
+++ app/models/MovieSchedule.php
@@ -9,11 +9,19 @@ class MovieSchedule extends \app\core\Model{
 	$day;
 
 	public function insert(){
-		$SQL = 'INSERT INTO movie_schedule(movie_id,day) VALUES (:movie_id,day)';
+		$SQL = 'INSERT INTO movie_schedule (movie_id,day) VALUES (:movie_id,day)';
 		$STMT = self::$_conn->prepare($SQL);
 		$data = ['movie_id'=>$this->movie_id,
 			'day'=>$this->day];
 		$STMT->execute($data);
+	}
+
+	public function delete($schedule_id){
+		$SQL = 'DELETE FROM movie_schedule WHERE schedule_id=:schedule_id';
+		$STMT = self::$_conn->prepare($SQL);
+		$STMT->execute(
+			['schedule_id'=>$schedule_id]
+		);
 	}
 
 	public function addAdmin(){

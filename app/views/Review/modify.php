ece6b6f57c2859c7beba595ab3483fd5b58ceeed
yumvinegar <horse.shoe@outlook.com>
2024-04-23 21:52:52 -0400

    put short base for some of the views

--- app/views/Review/modify.php
+++ app/views/Review/modify.php
@@ -1,4 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
+<head>
+    <title>Movie Theatre</title>
+    <style><?php include 'app/css/movie.css'; ?></style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</head>
+
+<!-- Navigation Bar -->
+<nav>
+    <a href="/User/profile">Account</a> &nbsp&nbsp
+    <a href="/Movie/index">Movies</a>
+</nav>
+
+<header>
+    <h1>Modify review</h1>
+</header>
+
+<a href="/User/profile">Cancel</a>
+
+
 
 </html>

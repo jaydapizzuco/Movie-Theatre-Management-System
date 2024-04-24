b68f781b3fdbbb77fcab9e71fb8e48e5e1f799bd
Jayda Pizzuco <jaydapizzuco@gmail.com>
2024-04-23 21:20:49 -0400

    updated styling of adminIndex

--- app/views/Movie/adminIndex.php
+++ app/views/Movie/adminIndex.php
@@ -23,18 +23,22 @@
         <h1>Add Screening Times </h1>
     </header><br><br>
 
-    <div id="gallery">
+    <div style="text-align: center;">
     	<?php foreach ($data as $index => $movies) { 
                 if ($movies->status == 1){ ?>
-                    <a href="/Movie/individual?id=<?=$movies->movie_id ?>"><img src='<?= $movies->image ?>' class='zoom' width='200' height='200' alt='<?= $movies->image ?>'></a><br>
+                    <div style=" margin-bottom: 20px;">
+                    <a href="/Movie/individual?id=<?=$movies->movie_id ?>"><img src='<?= $movies->image ?>' class='zoom' width='200' height='200' alt='<?= $movies->image ?>'></a>
                     <a href='/MovieSchedule/create?id=<?= $movies->movie_id ?>'><i class="bi bi-clock"></i></a>
                     <a href='/Movie/update?id=<?= $movies->movie_id ?>'><i class='bi-pencil-fill'></i></a>
                     <a href='/Movie/delete?id=<?= $movies->movie_id ?>'><i class='bi bi-trash'></i></a>
                     <br>
+                    </div>
     		<?php } else { ?>
+                 <div style=" margin-bottom: 20px">
                 <a href="/Movie/individual?id=<?=$movies->movie_id ?>"><img src='<?= $movies->image ?>' class='zoom' width='100' height='100' alt='<?= $movies->image ?>'></a>
                 <a href='/Movie/activate?id=<?= $movies->movie_id ?>'><i class='bi bi-arrow-repeat'></i></a>
                 <br>
+            </div>
                 <?php } } ?>
     </div>
 

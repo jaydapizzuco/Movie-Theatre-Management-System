e62a283c6ff8ee857d08e2440e158c6e52dc2b92
Jayda Pizzuco <jaydapizzuco@gmail.com>
2024-05-03 13:53:41 -0400

    got rid of one of the switch cases that determins today's date as an int, used date format N

--- app/views/Ticket/seatSelection.php
+++ app/views/Ticket/seatSelection.php
@@ -34,36 +34,7 @@
 
             $timestamp = date("Y-m-d");
             $timestamp = strtotime($timestamp);
-            $day = date('l', $timestamp);
-
-            $dayInt = 0;
-
-            switch ($day) {
-                case 'Sunday':
-                    $dayInt = 0;
-                    break;
-                case 'Monday':
-                    $dayInt = 1;
-                    break;
-                case 'Tuesday':
-                    $dayInt = 2;
-                    break;
-                case 'Wednesday':
-                    $dayInt = 3;
-                    break;
-                case 'Thursday':
-                    $dayInt = 4;
-                    break;
-                case 'Friday':
-                    $dayInt = 5;
-                    break;
-                case 'Saturday':
-                    $dayInt = 6;
-                    break;
-                
-                default:
-                    break;
-            }
+            $dayInt = date('N', $timestamp);
 
 
         //get index of movie day

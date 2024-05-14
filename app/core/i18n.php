cb8e56fc973975a4499bf60a1f008c1e879b8a26
Melissa <112568989+melissaweller@users.noreply.github.com>
2024-05-14 12:11:34 -0400

    added language buttons in nav bar of the homepage

--- app/core/i18n.php
+++ app/core/i18n.php
@@ -1,38 +1,45 @@
 <?php
-//use statements to simplify code below
-use Symfony\Component\Translation\Translator;
-use Symfony\Component\Translation\Loader\MoFileLoader;
-//list the localisations that have been built
-$supportedLocales = ['fr', 'en'];
-//get the requested locale to use here we default to 'fr'
-$locale = $_GET['lang'] ?? $_COOKIE['lang'] ?? 'en';
-//ensure the locale is supported
-if(!in_array($locale, $supportedLocales))
-	$locale = 'en';
-//save the setting to a cookie
-setcookie('lang',$locale,0,'/');
-//initialise a translator object for the locale
-$t = new Translator($locale);
-//add a loader for .mo files
-$t->addLoader('mo', new MoFileLoader());
-//grab the .mo file resource from the folders previously built
-$t->addResource('mo',"./locales/$locale/LC_MESSAGES/messages.mo", $locale);
+    //use statements to simplify code below
+    use Symfony\Component\Translation\Translator;
+    use Symfony\Component\Translation\Loader\MoFileLoader;
 
-//define a helper function to load translations from the translator
-//that remains unobstrusive for writing in views
-function __($message){
-	global $t;
-	return $t->trans($message);
+    //list the localisations that have been built
+    $supportedLocales = ['fr', 'en'];
 
+    //get the requested locale to use here we default to 'fr'
+    $locale = $_GET['lang'] ?? $_COOKIE['lang'] ?? 'en';
 
-	if (isset($_GET['lang'])) {
-    $requestedLocale = $_GET['lang'];
-    if (in_array($requestedLocale, $supportedLocales)) {
-        $locale = $requestedLocale;
-        setcookie('lang', $locale, 0, '/');
-        // Redirect back to the current page after changing language
-        header("Location: {$_SERVER['PHP_SELF']}");
-        exit();
-    }
-}
-}
+    //ensure the locale is supported
+    if(!in_array($locale, $supportedLocales))
+    	$locale = 'en';
+
+    //save the setting to a cookie
+    setcookie('lang',$locale,0,'/');
+
+    //initialise a translator object for the locale
+    $t = new Translator($locale);
+
+    //add a loader for .mo files
+    $t->addLoader('mo', new MoFileLoader());
+
+    //grab the .mo file resource from the folders previously built
+    $t->addResource('mo',"./locales/$locale/LC_MESSAGES/messages.mo", $locale);
+
+    //define a helper function to load translations from the translator
+    //that remains unobstrusive for writing in views
+    function __($message){
+    	global $t;
+    	return $t->trans($message);
+
+    	if (isset($_GET['lang'])) {
+            $requestedLocale = $_GET['lang'];
+
+            if (in_array($requestedLocale, $supportedLocales)) {
+                $locale = $requestedLocale;
+                setcookie('lang', $locale, 0, '/');
+                // Redirect back to the current page after changing language
+                header("Location: {$_SERVER['PHP_SELF']}");
+                exit();
+            }
+        }
+    }   
--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -7,7 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-
 </head>
 
 <nav>
@@ -16,9 +15,15 @@
     <a name ="aboutus" href="/Main/aboutus"><?= __('About us') ?></a> &nbsp&nbsp
     <a name="movies" href="/Movie/index"><?= __('Movies') ?></a> &nbsp&nbsp
     <a href ="/Order/incomplete"><i class="bi bi-cart"></i></a> &nbsp&nbsp
-</nav>
 
+    <form action="" class="form "method="GET" id="languageForm">
+        <button class="langButton" type="submit" name="lang" value="en"><?= __('English') ?></button>
+        <button class="langButton" type="submit" name="lang" value="fr"><?= __('French') ?></button>
+    </form>
+</nav>
 <body class="body">
+
+
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
@@ -57,12 +62,7 @@
 </body>
 
 <footer>
-    
-<form action="" method="GET" id="languageForm">
-    <button type="submit" name="lang" value="en"><?= __('English') ?></button>
-    <button type="submit" name="lang" value="fr"><?= __('French') ?></button>
-</form>
-        <br><?= __('Copyright &copy 2024') ?>
+    <br><?= __('Copyright &copy 2024') ?>
 </footer>
 
 </html>
